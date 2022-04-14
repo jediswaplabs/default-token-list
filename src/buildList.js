5a41eff05b0827234d6e9d947f29ab7898063af0
dhruvkelawala <dhruvrk2000@gmail.com>
2022-04-14 17:21:43 +0530

    Uniswap -> Jediswap

--- src/buildList.js
+++ src/buildList.js
@@ -1,32 +1,33 @@
 const { version } = require("../package.json");
 const mainnet = require("./tokens/mainnet.json");
-const ropsten = require("./tokens/ropsten.json");
-const rinkeby = require("./tokens/rinkeby.json");
 const goerli = require("./tokens/goerli.json");
-const kovan = require("./tokens/kovan.json");
-const polygon = require("./tokens/polygon.json");
-const mumbai = require("./tokens/mumbai.json");
+const { validateAndParseAddress } = require("starknet");
 
 module.exports = function buildList() {
   const parsed = version.split(".");
   return {
-    name: "Uniswap Labs List",
+    name: "Jediswap Labs List",
     timestamp: new Date().toISOString(),
     version: {
       major: +parsed[0],
       minor: +parsed[1],
-      patch: +parsed[2],
+      patch: +parsed[2]
     },
     tags: {},
     logoURI: "ipfs://QmNa8mQkrNKp1WEEeGjFezDmDeodkWRevGFN8JCV7b4Xir",
-    keywords: ["uniswap", "default"],
-    tokens: [...mainnet, ...ropsten, ...goerli, ...kovan, ...rinkeby, ...polygon, ...mumbai]
+    keywords: ["jediswap", "default"],
+    tokens: [...mainnet, ...goerli]
+      // parse address
+      .map(token => ({
+        ...token,
+        address: validateAndParseAddress(token.address)
+      }))
       // sort them by symbol for easy readability
       .sort((t1, t2) => {
         if (t1.chainId === t2.chainId) {
           return t1.symbol.toLowerCase() < t2.symbol.toLowerCase() ? -1 : 1;
         }
         return t1.chainId < t2.chainId ? -1 : 1;
-      }),
+      })
   };
 };
